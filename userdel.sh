4c76f21be825e09410dfdc674c216277384a1f06
Tworzyk <115981677+Tworzyk@users.noreply.github.com>
2025-02-07 22:20:16 +0100

    Fix issues

--- userdel.sh
+++ userdel.sh
@@ -3,7 +3,7 @@
 index=0
 declare -a names
 
-cat users.txt | awk '{print $3}' >> usertodel.txt
+cat users.txt | awk '{print $3}' > usertodel.txt
 
 while read -r name
 do
@@ -13,6 +13,9 @@ done < "usertodel.txt"
 
 for (( i = 0; i < $index; i++ ));
 do
-        userdel ${names[$i]}
+        if id "${names[$i]}" &>/dev/null; then
+                userdel -r ${names[$i]}
+        else
+                echo "User ${names[$i]} does not exist."
+        fi
 done
-echo "Users deleted successfully "
